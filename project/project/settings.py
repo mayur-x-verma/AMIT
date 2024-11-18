b4c984147d3de052e2a114ccbe48d34c3a886530
Mayur verma <115682497+mayur-x-verma@users.noreply.github.com>
2024-11-18 12:36:12 +0530

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ptvqqvcr^fhs&i=ei6@ltv5gy3k%vgpa_4p%-q#^(^l+ilb17n'
+SECRET_KEY = 'your_secret_key_here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True #False
@@ -102,7 +102,7 @@ DATABASES = {
         'ENGINE': 'mssql',
         'NAME': 'AMIT_DB',
         'USER': 'amitservicetechsa',
-        'PASSWORD': 'NGSU*gm3RAxMzUto5F#',
+        'PASSWORD': '********************',
         'HOST': '202.66.174.110,1232',
         'PORT': '',
 
@@ -171,7 +171,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'amtechnologiesvns@gmail.com'
-EMAIL_HOST_PASSWORD = 'fhab zzsx kmel bxkl'
+EMAIL_HOST_PASSWORD = '*************'
 # DEFAULT_FROM_EMAIL = 'mayurverma619@gmail.com'
 
 
